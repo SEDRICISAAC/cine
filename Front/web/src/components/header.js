53d8b1b788d9282a23bd2dd05e2d6c83dce5fae8
Entx <49497963+Entx@users.noreply.github.com>
2020-02-09 21:36:39 -0500

    web

--- Front/web/src/components/header.js
+++ Front/web/src/components/header.js
@@ -2,9 +2,44 @@
 import React from'react';
 import { Link } from "react-router-dom";
 
+
 const Header = () => (
-        <div className="flex-grow flex mr-2 ml-64">
+        <div className="flex-grow flex mr-2 ml-0">
             <div className="flex-grow flex">
+                {/* modificadooo flacaaaaa */}
+            <nav class="bg-gray-800 p-2 mt-0 fixed w-full z-10 top-0">
+        <div class="container mx-auto flex flex-wrap items-center">
+		    <div class="flex w-full md:w-1/2 justify-center md:justify-start text-white font-extrabold">
+				<a class="text-white no-underline hover:text-white hover:no-underline" href="#">
+					<span class="text-2xl pl-2"><i class="em em-grinning"></i> Brand McBrandface</span>
+				</a>
+            </div>
+			<div class="flex w-full pt-2 content-center justify-between md:w-1/2 md:justify-end">
+				<ul class="list-reset flex justify-between flex-1 md:flex-none items-center">
+				  <li class="mr-3">
+                  <Link to="/">
+                    <button className="inline-block py-2 px-4 text-white no-underline">
+                      Salir
+                    </button>
+                </Link>
+					<a class="inline-block py-2 px-4 text-white no-underline" href="#">Active</a>
+				  </li>
+				  <li class="mr-3">
+					<a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">link</a>
+				  </li>
+				  <li class="mr-3">
+					<a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">link</a>
+				  </li>
+					<li class="mr-3">
+					<a class="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4" href="#">link</a>
+				  </li>
+				</ul>
+			</div>
+        </div>
+    </nav>
+                {/* modificadooo flacaaaaa */}
+
+
                 {/* <a className="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
                     Ayuda
                 </a> */}
